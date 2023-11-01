b721888eec8eccbefbe657bcc526bb01810b2dda
anvari <zahraanvari216@gmail.com>
2023-11-01 08:21:07 +0330

    route the component

--- front/src/components/todo/Todo.jsx
+++ front/src/components/todo/Todo.jsx
@@ -1,21 +1,53 @@
 import { useState } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './Todo.css';
 
 export default function Todo() {
 
+    return(
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='' element={<LoginComponent />}></Route>
+                    <Route path='' element={<WelcomeComponent />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+
+}
+
+function LoginComponent() {
     const [username, setUsername] = useState("admin")
+    const [password, setPassword] = useState("123456")
+
+    const [successMessage, setSuccessMessage] = useState(false)
+    const [errorMessage, setErrorMessage] = useState(false)
+
 
     function handelChangeUsername(event) {
         setUsername(event.target.value)
     }
 
-    const [password, setPassword] = useState("123456")
 
     function handelChangePassword(event) {
         setPassword(event.target.value)
     }
+
+    function handelLogin() {
+        if(username === 'admin' && password === 'pass') {
+            setSuccessMessage(true)
+            setErrorMessage(false)
+        }else{
+            setSuccessMessage(false)
+            setErrorMessage(true)
+        }
+    }
+
     return(
         <div>
+            {successMessage && <div className='successMessage'>Authenticated successfuly!</div>}
+            {errorMessage && <div className='errorMessage'>Authentication failed. Please check your credentials</div>}
             <div>
                 <label>Username</label>
                 <input type="text" name="username" value={username} onChange={handelChangeUsername}/>
@@ -25,8 +57,16 @@ export default function Todo() {
                 <input type="password" name="password" value={password} onChange={handelChangePassword}/>
             </div>
             <div>
-                <button type="button">login</button>
+                <button type="button" onClick={handelLogin}>login</button>
             </div>
+        </div>
+    )
+}
+
+function WelcomeComponent() {
+    return(
+        <div>
+            Welcome Component
         </div>
     )
 }
